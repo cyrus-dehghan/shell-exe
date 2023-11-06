0804763981c68838408032b81222197fd075430a
cyrus-dehghan <146709651+cyrus-dehghan@users.noreply.github.com>
2023-11-06 10:57:59 +0100

    Delete Job08 directory

--- Job08/get_logs.sh
+++
@@ -1,7 +0,0 @@
-file=number_connection-$(date +"%d-%m-%y-%R")
-user="cyrus"
-
-last | grep $user | wc -l > $file
-tar -cvf "/home/cyrus/Documents/shell-exe/job08/$file.tar" "$file"
-mv "/home/cyrus/Documents/shell-exe/job08/$file.tar" "/home/cyrus/Documents/shell-exe/Job08/Backup"
-rm "/home/cyrus/Documents/shell-exe/job08/$file"
